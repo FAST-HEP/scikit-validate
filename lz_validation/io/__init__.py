42f8f4614243f7d18b1b9041c5120629d2b74e1d
kreczko <lkreczko@googlemail.com>
2018-08-10 14:28:57 +0100

    added io.root_walk to traverse through a ROOT file and list all branches/leafes

--- lz_validation/io/__init__.py
+++ lz_validation/io/__init__.py
@@ -9,3 +9,12 @@ def save_metrics_to_file(metrics, metrics_file):
             metrics.update(json.load(f))
     with open(metrics_file, 'w') as f:
         json.dump(metrics, f)
+
+
+def root_walk(obj):
+    for k in sorted(obj.allkeys()):
+        if not obj[k].fBranches:
+            yield k.decode("utf-8"), obj[k]
+        else:
+            for name, branch in root_walk(obj[k]):
+                yield '{0}.{1}'.format(k.decode("utf-8"), name), branch
