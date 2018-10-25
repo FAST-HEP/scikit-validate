a7989c6b898029cadf53b2e27dade046f2b54f6c
kreczko <lkreczko@googlemail.com>
2018-10-25 15:59:48 +0100

    updated io.walk to handle & unpack objects

--- skvalidate/io/__init__.py
+++ skvalidate/io/__init__.py
@@ -15,16 +15,39 @@ def save_metrics_to_file(metrics, metrics_file):
 
 def walk(path_to_root_file):
     f = uproot.open(path_to_root_file)
-    return _walk(f)
+    for name, obj in _walk(f):
+        for subname, subobj in unpack(name, obj):
+            yield subname, subobj
 
 
 def _walk(obj):
-    for k in sorted(obj.allkeys()):
-        try:
-            yield k.decode("utf-8"), obj[k].array().flatten()
-        except Exception:
-            for n, o in _walk(obj[k]):
-                yield '{0}.{1}'.format(k.decode("utf-8"), n), o
+    seen = set()
+
+    def _step(obj):
+        for k in sorted(obj.allkeys()):
+            value = obj[k]
+            if hasattr(value, '_streamer') and 'TStreamerInfo' not in value._streamer.__class__.__name__:
+                obj_id = id(value._streamer)
+                if obj_id in seen and value._streamer is not None:
+                    continue
+                seen.add(obj_id)
+                yield k.decode("utf-8"), value
+            else:
+                for n, o in _step(value):
+                    yield '{0}.{1}'.format(k.decode("utf-8"), n), o
+    return _step(obj)
+
+
+def unpack(name, obj):
+    flat_array = obj.array(flatten=True)
+
+    if flat_array.__class__.__name__ == 'ObjectArrayMethods':
+        o = flat_array[0]
+        attributes = [x for x in dir(o) if x.startswith('_f')]
+        for a in attributes:
+            yield name + '.' + a, np.asarray([getattr(x, a) for x in flat_array])
+    else:
+        yield name, flat_array
 
 
 def save_array_to_file(array, name, output_dir):
--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from skvalidate.io import walk
+from skvalidate.io import walk, unpack
 
 
 @pytest.mark.parametrize("input_file,names,types", [
@@ -19,9 +19,28 @@ from skvalidate.io import walk
         ['test;1.i', 'test;1.x', 'test;1.y', 'test;1.z', 'test;1.v', 'test;1.a'],
         [np.int32, np.float32, np.float32, np.float32, np.float32, np.float32]
     ),
+    (
+        'tests/samples/objects.root',
+        [
+            'Events;1.MyEvent.TObject.fUniqueID', 'Events;1.MyEvent.TObject.fBits', 'Events;1.MyEvent.eventID',
+            'Events;1.MyEvent.ayes', 'Events;1.MyEvent.ayes.start_ns', 'Events;1.MyEvent.ayes.end_ns',
+            'Events;1.MyEvent.bees', 'Events;1.MyEvent.bees.driftTime',
+            'Events;1.MyEvent.bees.xyPosition._fX', 'Events;1.MyEvent.bees.xyPosition._fY',
+            'Events;1.MyEvent.bees.xyzPosition._fX', 'Events;1.MyEvent.bees.xyzPosition._fY',
+            'Events;1.MyEvent.bees.xyzPosition._fZ',
+        ],
+        [
+            np.uint32, np.uint32, np.uint32,
+            np.int32, np.float32, np.float32,
+            np.int32, np.float32,
+            np.float64, np.float64,
+            np.float64, np.float64, np.float64
+        ]
+    ),
 ])
 def test_walk(input_file, names, types):
     result = list(walk(input_file))
+    assert len(result) == len(names)
 
     for name, array in result:
         assert name in names
