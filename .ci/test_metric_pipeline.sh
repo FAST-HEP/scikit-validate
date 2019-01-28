d3c468b403f1b588847dcef9cda5a3e7cc65e93c
kreczko <lkreczko@googlemail.com>
2019-01-28 16:27:13 +0000

    added validation of the metrics pipeline (run & file metrics -> report) to the CI

---
+++ .ci/test_metric_pipeline.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# reference run
+dd if=/dev/zero of=file1 bs=1048576 count=10
+dd if=/dev/zero of=file2 bs=1048576 count=42
+add_file_metrics file1 file2 --metrics-file=file_metrics_ref.json
+
+execute_with_metrics \
+  'stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose' \
+  -m metrics_ref.json
+
+rm -f file1 file2
+
+# validation run
+dd if=/dev/zero of=file1 bs=1048576 count=12
+dd if=/dev/zero of=file2 bs=1048576 count=20
+add_file_metrics file1 file2 --metrics-file=file_metrics.json
+
+execute_with_metrics \
+  'stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose' \
+  -m metrics.json
+
+rm -f file1 file2
+
+# prepare report
+cp -vp *metrics*.json skvalidate/data/examples/.
