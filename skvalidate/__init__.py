87dba6e5af66abbc02546e8c798466a595aa70e4
kreczko <lkreczko@googlemail.com>
2019-01-28 12:28:49 +0000

    changed version: 0.1.8 --> 0.2.0rc1

--- skvalidate/__init__.py
+++ skvalidate/__init__.py
@@ -5,6 +5,6 @@ import os
 
 __author__ = """FAST"""
 __email__ = 'fast-hep@cern.ch'
-__version__ = '0.1.8'
+__version__ = '0.2.0rc1'
 
 __skvalidate_root__ = os.path.join(os.path.dirname(__file__), '..')
