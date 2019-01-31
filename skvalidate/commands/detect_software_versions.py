ce77a5cf960ff19c27c5256c6a68b0c43c0ce973
kreczko <lkreczko@googlemail.com>
2019-01-31 15:30:20 +0000

    added quiet option for detect_software_versions

--- skvalidate/commands/detect_software_versions.py
+++ skvalidate/commands/detect_software_versions.py
@@ -11,16 +11,20 @@ from skvalidate.io import update_data_in_json
 @click.argument('software', nargs=-1, required=True)
 @click.option('-o', '--output', default='software_versions.json', type=click.Path())
 @click.option('-p', '--prefix', default='build', type=click.Path())
-def cli(software, output, prefix):
+@click.option('-q', '--quiet', count=True)
+def cli(software, output, prefix, quiet):
     software_versions = {}
     software_versions[prefix] = {}
     for s in software:
-        print('>> Detecting version for:', s)
+        if quiet < 1:
+            print('>> Detecting version for:', s)
         try:
             version = get_software_version(s)
-            print('>>>> Found {} version {}'.format(s, version))
+            if quiet < 1:
+                print('>>>> Found {} version {}'.format(s, version))
             software_versions[prefix][s] = version
         except KeyError as e:
-            print(colors.red | str(e.message))
+            if quiet < 2:
+                print(colors.red | str(e.message))
     if software_versions[prefix]:
         update_data_in_json(software_versions, output)
