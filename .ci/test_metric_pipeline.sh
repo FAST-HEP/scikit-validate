443d943b6556c931ea1e4f20b7017b0bcbc6a410
Lukasz Kreczko <lkreczko@googlemail.com>
2019-03-13 18:06:45 -0600

    using a single profile file for memory_profile

--- .ci/test_metric_pipeline.sh
+++ .ci/test_metric_pipeline.sh
@@ -9,12 +9,12 @@ dd if=/dev/zero of=file2 bs=1048576 count=20
 sv_file_info file1 file2 --metrics-file=${OUT_DIR}/file_metrics.json
 
 sv_execute -m performance_metrics.json \
-  -m ${OUT_DIR}/performance_metrics.json --memprof-file ${OUT_DIR}/stress_1.dat \
+  -m ${OUT_DIR}/performance_metrics.json --memprof-file ${OUT_DIR}/stress.dat \
   -- \
   stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose
 
 sv_execute -m performance_metrics.json \
-  -m ${OUT_DIR}/performance_metrics.json --memprof-file ${OUT_DIR}/stress_2.dat \
+  -m ${OUT_DIR}/performance_metrics.json --memprof-file ${OUT_DIR}/stress.dat \
   -- \
   stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose
 
