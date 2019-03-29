a65d060df0a391c6e62c5c77428e5c1383d9057f
Ben Krikler <bek07@ic.ac.uk>
2019-03-29 23:45:52 +0100

    Use new command-line style in ci

--- .ci/test_metric_pipeline.sh
+++ .ci/test_metric_pipeline.sh
@@ -5,13 +5,11 @@ dd if=/dev/zero of=file1 bs=1048576 count=10
 dd if=/dev/zero of=file2 bs=1048576 count=42
 sv_file_info file1 file2 --metrics-file=file_metrics_ref.json
 
-sv_execute \
-  'stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose' \
-  -m performance_metrics_ref.json
+sv_execute -m performance_metrics_ref.json -- \
+  stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose
 
-sv_execute \
-    'stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose' \
-    -m performance_metrics_ref.json
+sv_execute -m performance_metrics_ref.json -- \
+    stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose
 
 rm -f file1 file2
 
@@ -20,13 +18,11 @@ dd if=/dev/zero of=file1 bs=1048576 count=12
 dd if=/dev/zero of=file2 bs=1048576 count=20
 sv_file_info file1 file2 --metrics-file=file_metrics.json
 
-sv_execute \
-  'stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose' \
-  -m performance_metrics.json
+sv_execute -m performance_metrics.json -- \
+  stress --cpu 1 --io 1 --vm 1 --vm-bytes 128M --timeout 10s --verbose
 
-sv_execute \
-    'stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose' \
-    -m performance_metrics.json
+sv_execute -m performance_metrics.json -- \
+    stress --cpu 1 --io 1 --vm 1 --vm-bytes 200M --timeout 15s --verbose
 
 rm -f file1 file2
 
