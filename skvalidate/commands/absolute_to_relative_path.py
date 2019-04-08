68ad9b495211bff60aba3c2031ad59851b0b9546
kreczko <lkreczko@googlemail.com>
2019-04-08 11:14:41 +0100

    added new command: sv_absolute_to_relative_path

---
+++ skvalidate/commands/absolute_to_relative_path.py
@@ -0,0 +1,16 @@
+"""
+Command that translates absolute paths to relative paths.
+
+Meant to provide functionality of `realpath` for older platforms.
+"""
+import os
+
+import click
+
+@click.command(help=__doc__)
+@click.argument('absolute_path', type=click.Path())
+@click.option('--relative-to', default=os.getcwd(), type=click.Path())
+def cli(absolute_path, relative_to):
+    relative_path = os.path.relpath(absolute_path, relative_to)
+    relative_path = os.path.normpath(relative_path)
+    click.echo(relative_path)
