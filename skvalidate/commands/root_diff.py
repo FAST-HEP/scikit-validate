3f3f23accede4906e8d67fdffb488f2538c4ef01
kreczko <lkreczko@googlemail.com>
2019-04-08 15:49:58 +0100

    sv_root_diff: added multi-processing support

--- skvalidate/commands/root_diff.py
+++ skvalidate/commands/root_diff.py
@@ -6,6 +6,7 @@ TODO: separate functionality: plotting, recursive reading of ROOT files, diff ca
 TODO: allow for injection of user-defined high-level variables
 """
 from __future__ import print_function
+import multiprocessing as mp
 import os
 import threading
 
@@ -19,34 +20,94 @@ from skvalidate.vis import draw_diff
 from skvalidate.io import write_data_to_json
 
 
-class ProcessStatus(threading.Thread):
+def _process(name, values, output_path):
+    color = colors.red
+    msg = compare.ERROR
 
-    def __init__(self, name, values, output_path):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.values = values
+    evaluationValue = values['evaluationValue']
+    status = values['status']
+
+    if status == compare.FAILED:
+        image = draw_diff(name, values, output_path)
+        values['image'] = image
+        msg = 'FAILED (test: {:0.3f}): {}'.format(evaluationValue, image)
+    if status == compare.UNKNOWN:
+        msg = 'WARNING: Unable to compare (value type: {0}, reason: {1})'.format(
+            values['original'].dtype,
+            values['reason'],
+        )
+        color = colors.Orange3
+    if status == compare.SUCCESS:
+        msg = 'OK'
+        color = colors.green
+    values['msg'] = msg
+
+    print(color | '{0} - {1}'.format(name, msg))
+
+    del values['original']
+    del values['reference']
+    del values['diff']
+
+    return values
+
+
+class MultiProcessStatus(object):
+
+    def __init__(self, comparison, n_processes, output_path):
+        self.comparison = comparison
+        self.pool = mp.Pool(n_processes)
+        self.n_cores = n_processes
         self.output_path = output_path
-        self.color = colors.red
-        self.msg = compare.ERROR
+        self.n_comparisons = len(comparison)
+        self.pbar = tqdm(self.n_comparisons)
+
+    def runMultiProcessing(self):
+        print('Testing {0} distributions'.format(self.n_comparisons))
+        results = {}
+        for name in sorted(self.comparison.keys()):
+            result = self.pool.apply_async(
+                _process,
+                args=(name, self.comparison[name], self.output_path),
+                callback=self._update,
+            )
+            results[name] = result
+        self.pool.close()
+        self.pool.join()
+        for name in results.keys():
+            self.comparison[name] = results[name].get()
+
+    def runSingleCore(self):
+        print('Testing {0} distributions'.format(self.n_comparisons))
+        for name in sorted(self.comparison.keys()):
+            result = self.__process(name, self.comparison[name], self.output_path)
+            self.comparison[name] = result
 
     def run(self):
-        evaluationValue = self.values['evaluationValue']
-        status = self.values['status']
-        if status == compare.FAILED:
-            image = draw_diff(self.name, self.values, self.output_path)
-            self.values['image'] = image
-            self.msg = 'FAILED (test: {:0.3f}): {}'.format(evaluationValue, image)
-        if status == compare.UNKNOWN:
-            self.msg = 'WARNING: Unable to compare (value type: {})'.format(self.values['original'].dtype)
-            self.color = colors.Orange3
-        if status == compare.SUCCESS:
-            self.msg = 'OK'
-            self.color = colors.green
-        self.values['msg'] = self.msg
-        del self.values['original']
-        del self.values['reference']
-        del self.values['diff']
+        if self.n_cores > 1:
+            self.runMultiProcessing()
+        else:
+            self.runSingleCore()
 
+    def runMultiThread(self):
+        print('Testing {0} distributions'.format(self.n_comparisons))
+        threads = {}
+        for name in sorted(self.comparison.keys()):
+            thread = threading.Thread(target=self.__process, args=(name, self.comparison[name], self.output_path))
+            thread.daemon = True
+            thread.start()
+            threads[name] = thread
+
+        for name, thread in threads.items():
+            thread.join()
+            self._update()
+            self.comparison[name] = thread.values
+
+    def _update(self, *a):
+        self.pbar.update()
+
+    def terminate(self):
+        self.pool.terminate()
+        self.pool.join()
 
 @click.command()
 @click.argument('file_under_test', type=click.Path(exists=True))
@@ -54,27 +115,21 @@ class ProcessStatus(threading.Thread):
 @click.option('-o', '--output-path', type=click.Path(exists=True), required=True)
 @click.option('-r', '--report-file', type=click.Path(), default='root_comparison.json')
 @click.option('-p', '--prefix', default=os.environ.get('CI_JOB_NAME', 'root_diff'))
-def cli(file_under_test, reference_file, output_path, report_file, prefix):
+@click.option('-n', '--n-cores', default=1, type=int, help='Experimental feature: use n number of cores')
+def cli(file_under_test, reference_file, output_path, report_file, prefix, n_cores):
     # TODO add verbosity setting
     # TODO: add parameter for distributions that are allowed to file (e.g. timestamps)
     # TODO: throw error if any distribution fails
     comparison = compare.compare_two_root_files(file_under_test, reference_file)
     comparison = _reset_infinities(comparison)
 
-    print('Testing {0} distributions'.format(len(comparison)))
-    threads = {}
-    for name in sorted(comparison.keys()):
-        values = comparison[name]
-        thread = ProcessStatus(name, values, output_path)
-        thread.start()
-        threads[name] = thread
+    processing = MultiProcessStatus(comparison, n_cores, output_path)
+    try:
+        processing.run()
+    except KeyboardInterrupt:
+        processing.terminate()
 
-    for name, thread in tqdm(threads.items()):
-        thread.join()
-        print(thread.color | '{0} - {1}'.format(thread.name, thread.msg))
-        comparison[name] = thread.values
-
-    summary = _add_summary(comparison, prefix)
+    summary = _add_summary(processing.comparison, prefix)
     summary[prefix]['output_path'] = output_path
     # TODO: print nice summary
     write_data_to_json(summary, report_file)
