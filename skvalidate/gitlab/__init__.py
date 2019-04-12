efce5da831f0d6a0bd78fe3869ef9ead04e7fd4b
kreczko <lkreczko@googlemail.com>
2019-04-12 16:31:12 +0100

    fixed issue #6: Bug: gitlab.get_pipeline_job returns first job only

--- skvalidate/gitlab/__init__.py
+++ skvalidate/gitlab/__init__.py
@@ -92,10 +92,10 @@ def get_jobs_for_stages(stages, **kwargs):
 
 def get_pipeline_job(job_name):
     jobs = _get_current_pipeline_jobs()
-    for job in jobs:
-        name = job.attributes['name']
-        if name == job_name:
-            return job
+    selected_jobs = [job for job in jobs if job_name == job.attributes['name']]
+    selected_jobs = sorted(selected_jobs, key=lambda j: j.id, reverse=True)
+    if selected_jobs:
+        return selected_jobs[0]  # first job should have highest ID --> latest
     return None
 
 
