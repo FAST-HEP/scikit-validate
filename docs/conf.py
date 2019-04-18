5cf57d45d529b976ea696f9ed1d3135e552b842d
kreczko <lkreczko@googlemail.com>
2019-04-18 16:38:47 +0100

    docs: switched from alabaster to sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -107,6 +107,7 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'skvalidatedoc'
 
+logo_only = True
 html_logo = '_static/sk_logo.png'
 
 # -- Options for LaTeX output ------------------------------------------
