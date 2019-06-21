22faaa102dfc15f8502869b1adad5100910c8964
kreczko <lkreczko@googlemail.com>
2019-06-21 09:47:44 +0100

    when getting pipeline jobs: filter for only the last iteration of a particular job

--- tests/gitlab/test_gitlab.py
+++ tests/gitlab/test_gitlab.py
@@ -1,8 +1,9 @@
 import os
 from collections import namedtuple
 import pytest
+import random
 
-from skvalidate.gitlab import path_and_job_id_to_artifact_url
+from skvalidate.gitlab import path_and_job_id_to_artifact_url, _select_last_iteration_only
 
 
 @pytest.fixture()
@@ -13,6 +14,17 @@ def ci_project():
         job_id=42,
         url='https://gitlab.example.com/group/MyProject'
     )
+
+
+@pytest.fixture()
+def ci_jobs():
+    Job = namedtuple('Job', ['id', 'name'])
+    jobs = []
+    for i in range(5):
+        jobs.append(Job(id=i + 100, name='build'))
+        jobs.append(Job(id=i + 200, name='test'))
+    random.shuffle(jobs)
+    return jobs
 
 
 @pytest.mark.parametrize('path,path_type,expected_url', [
@@ -42,3 +54,15 @@ def test_path_and_job_id_to_artifact_url(ci_project, path, path_type, expected_u
     os.environ['CI_PROJECT_URL'] = ci_project.url
     url = path_and_job_id_to_artifact_url(path, ci_project.job_id, path_type=path_type)
     assert url == expected_url
+
+
+def test_select_last_iteration_only(ci_jobs):
+    highest_build_id = max([j.id for j in ci_jobs if j.name == 'build'])
+    highest_test_id = max([j.id for j in ci_jobs if j.name == 'test'])
+    jobs = _select_last_iteration_only(ci_jobs)
+    assert len(jobs) == 2
+    for job in jobs:
+        if job.name == 'build':
+            assert job.id == highest_build_id
+        if job.name == 'test':
+            assert job.id == highest_test_id
