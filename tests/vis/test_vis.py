87c4f0edf2633fba587d32d95d40674702c28a3c
Luke Kreczko <lkreczko@googlemail.com>
2020-12-03 22:14:26 +0000

    added tests for drawing failures

--- tests/vis/test_vis.py
+++ tests/vis/test_vis.py
@@ -1,8 +1,9 @@
 from hypothesis import given, assume
 import hypothesis.strategies as st
 import pytest
+import numpy as np
 
-from skvalidate.vis import adjust_axis_limits
+from skvalidate.vis import adjust_axis_limits, draw_diff, find_limits
 
 
 @given(st.floats(min_value=-1e6, max_value=1e6), st.floats(min_value=-1e6, max_value=1e6))
@@ -46,3 +47,55 @@ def _test_string_adjust_axis_limits(a_min, a_max):
     logy = False
     change = 0.2
     pytest.raises(TypeError, adjust_axis_limits(a_min, a_max, change, logy))
+
+
+@pytest.mark.parametrize('original,reference,amin,amax', [
+    (
+        np.array([1, 2, 3, 4]),
+        np.array([1, 3, 4, 5]),
+        1,
+        5,
+    ),
+    (
+        np.array([1, 2, 3, 4]),
+        None,
+        1,
+        4,
+    ),
+    (
+        np.array([1, 2, 3, 4]),
+        [],
+        1,
+        4,
+    ),
+])
+def test_find_limits(original, reference, amin, amax):
+    l1, l2 = find_limits(original, reference)
+    assert l1 == amin
+    assert l2 == amax
+
+
+@pytest.mark.parametrize('original,reference,', [
+    (
+        np.array([1, 2, 3, 4]),
+        np.array([1, 3, 4, 5]),
+    ),
+    (
+        np.array([1, 2, 3, 4]),
+        None,
+    ),
+    (
+        np.array([1, 2, 3, 4]),
+        [],
+    ),
+])
+def test_draw_diff(original, reference):
+    name = 'test123'
+    output_path = '/tmp'
+    values = {
+        'original': original,
+        'reference': reference,
+        'ks_statistic': 0,
+        'pvalue': 0,
+    }
+    draw_diff(name, values, output_path)
